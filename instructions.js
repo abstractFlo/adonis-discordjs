16de7da5c114dd6ffeb4b1389c01d018ac569e15
Florian Uhlrich <f.uhlrich@abstractmedia.de>
2017-12-24 16:42:32 +0100

    Change instructions.js

--- instructions.js
+++ instructions.js
@@ -1,19 +1,18 @@
 'use strict'
 
 const path = require('path')
-const Helpers = use('Adonis/Src/Helpers')
 const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = async () => {
   try {
     const templateFile = path.join(__dirname, 'stubs', 'bot-plugins.js')
-    const copyPath = path.join(Helpers.appRoot(), 'bot-plugins.js')
+    const copyPath = path.join(process.cwd(), 'bot-plugins.js')
 
     let exists = await fs.existsSync(copyPath)
 
     if (!exists) {
-      await fs.copyFileSync(templateFile, path.join(Helpers.appRoot(), 'bot-plugins.js'))
+      await fs.copyFileSync(templateFile, path.join(process.cwd(), 'bot-plugins.js'))
       console.log(chalk.green('Bot Plugins File copy success'))
     } else {
       console.log(chalk.yellow('Bot Plugins File already exists'))
